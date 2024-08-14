9e41e5bff5ffaf7a7ccd3beb219464f7bee38149
Wunders149 <razafimahefaphilibert7@gmail.com>
2024-08-14 06:48:32 +0300

    first commit

---
+++ scripts/translate.js
@@ -0,0 +1,25 @@
+async function translateText(text, targetLang) {
+    const response = await fetch('https://fr.libretranslate.com/translate', {
+        method: 'POST',
+        body: JSON.stringify({
+            q: text,
+            source: 'en',
+            target: targetLang,
+            format: 'text'
+        }),
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    const data = await response.json();
+    return data.translatedText;
+}
+
+function translatePage(lang) {
+    const elements = document.querySelectorAll('.translate');
+    
+    elements.forEach(async function(el) {
+        const text = el.textContent;
+        const translatedText = await translateText(text, lang);
+        el.textContent = translatedText;
+    });
+}
